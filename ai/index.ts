f1a80f723eeb1def1713c8e7b61cefeeb09f4024
gostore0 <gostore0@gmail.com>
2025-04-10 09:56:43 +0200

    Update index.ts

--- ai/index.ts
+++ ai/index.ts
@@ -1,8 +1,8 @@
-import { openai } from "@ai-sdk/openai";
+import { google } from "@ai-sdk/google";
 import { experimental_wrapLanguageModel as wrapLanguageModel } from "ai";
 import { ragMiddleware } from "./rag-middleware";
 
 export const customModel = wrapLanguageModel({
-  model: openai("gpt-4o"),
+  model: google("gemini-2.0-flash-exp"),
   middleware: ragMiddleware,
 });
